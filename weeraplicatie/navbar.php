fc74ce2423132cb75cc783a0580bebb2d51b1a97
Ruben.W <9512358+ProAdmin007@users.noreply.github.com>
2021-02-04 22:52:49 +0100

    Oops spelling is latig

--- weeraplicatie/navbar.php
+++
@@ -1,4 +0,0 @@
-<a href="home.php">OVERVIEW</a>
-<!--<a href="download-data.php">DOWNLOAD DATA</a>-->
-<a href="users-list.php">MANAGEMENT</a>
-<a href="./includes/logout.inc.php" class="nav-r">SIGN OUT</a>
